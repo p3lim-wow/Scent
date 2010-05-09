d1eaacdbeb89d9784efbae28072eb166f3653c60
Adrian L Lange <adrianlund@gmail.com>
2010-05-10 00:39:05 +0200

    Add offhand aswell, and do it pretty

--- Temporary.lua
+++ Temporary.lua
@@ -1,30 +1,33 @@
-do
+local function GetEnchantInfo(id)
+	local _, mainhand, _, _, offhand = GetWeaponEnchantInfo()
+	if(id == 1) then
+		return mainhand
+	elseif(id == 2) then
+		return offhand
+	end
+end
+
+local function CreateTemp(id, point)
 	local frame = CreateFrame('Button', nil, Minimap)
-	frame:SetPoint('BOTTOMLEFT')
+	frame:SetPoint('BOTTOM'..point)
 	frame:SetSize(10, 30)
 	frame:Hide()
 
 	local text = frame:CreateFontString(nil, 'ARTWORK')
 	text:SetAllPoints()
 	text:SetFont([=[Interface\AddOns\oUF_P3lim\media\semplice.ttf]=], 9, 'OUTLINE')
-	text:SetJustifyH('LEFT')
+	text:SetJustifyH(point)
 
+	frame:SetScript('OnClick', function() CancelItemTempEnchantment(id) end)
 	frame:SetScript('OnUpdate', function(self, elapsed)
-		if(self.elapsed and self.elapsed > 1) then
-			local enchant, time = GetWeaponEnchantInfo()
-			if(enchant) then
-				local str, val = SecondsToTimeAbbrev(time / 1000)
-				text:SetFormattedText(str:gsub(' ', ''), val)
-			end
+		if((self.elapsed or 6) > 5) then
+			local str, val = SecondsToTimeAbbrev(GetEnchantInfo(id) / 1e3)
+			text:SetFormattedText(str:gsub(' ', ''), val)
 
 			self.elapsed = 0
 		else
-			self.elapsed = (self.elapsed or 0) + elapsed
+			self.elapsed = self.elapsed + elapsed
 		end
-	end)
-
-	frame:SetScript('OnClick', function(self, button)
-		CancelItemTempEnchantment(1)
 	end)
 
 	frame:RegisterEvent('PLAYER_LOGIN')
@@ -32,12 +35,17 @@ do
 	frame:SetScript('OnEvent', function(self, event, unit)
 		if(unit and unit ~= 'player') then return end
 
-		if(GetWeaponEnchantInfo()) then
+		if(GetEnchantInfo(id)) then
 			self:Show()
 		else
 			self:Hide()
 		end
 	end)
+end
+
+do
+	CreateTemp(1, 'LEFT')
+	CreateTemp(2, 'RIGHT')
 
 	TemporaryEnchantFrame:Hide()
 end
