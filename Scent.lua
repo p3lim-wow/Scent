f45f873c6741396a1f1a6fe172d7e442658b4fbe
Adrian L Lange <adrianlund@gmail.com>
2011-05-22 03:45:27 +0200

    Some cleaning is needed

--- Scent.lua
+++ Scent.lua
@@ -1,73 +1,72 @@
 
+local FONT = [=[Interface\AddOns\Scent\semplice.ttf]=]
+local BACKDROP = {
+	bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=],
+	insets = {top = -1, bottom = -1, left = -1, right = -1}
+}
+
 local function UpdateTime(self, elapsed)
 	if(self.expiration) then
 		self.expiration = math.max(self.expiration - elapsed, 0)
+
 		if(self.expiration <= 0 or self.expiration > 90) then
-			self.time:SetText('')
+			self.Duration:SetText('')
 		else
-			self.time:SetText(math.floor(self.expiration))
+			self.Duration:SetText(math.floor(self.expiration))
 		end
 	end
 end
 
-local function UpdateAuras(header, button)
-	if(not button.texture) then
-		button.texture = button:CreateTexture(nil, 'BORDER')
-		button.texture:SetAllPoints()
-
-		button.count = button:CreateFontString(nil, 'ARTWORK')
-		button.count:SetPoint('BOTTOMRIGHT', -1, 1)
-		button.count:SetFont([=[Interface\AddOns\Scent\semplice.ttf]=], 8, 'OUTLINEMONOCHROME')
-
-		button.time = button:CreateFontString(nil, 'ARTWORK')
-		button.time:SetPoint('TOPLEFT', 1, -1)
-		button.time:SetFont([=[Interface\AddOns\Scent\semplice.ttf]=], 8, 'OUTLINEMONOCHROME')
-
-		button:SetScript('OnUpdate', UpdateTime)
-		button:SetBackdrop({bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=], insets = {top = -1, bottom = -1, left = -1, right = -1}})
-		button:SetBackdropColor(0, 0, 0)
-	end
-
-	local name, _, texture, count, dtype, duration, expiration = UnitAura(header:GetAttribute('unit'), button:GetID(), header:GetAttribute('filter'))
+local function UpdateAuras(self, index)
+	local name, _, texture, count, dtype, duration, expiration = UnitAura(self:GetParent():GetAttribute('unit'), index, self.filter)
 	if(name) then
-		button.texture:SetTexture(texture)
-		button.texture:SetTexCoord(0.08, 0.92, 0.08, 0.92)
-		button.count:SetText(count > 1 and count or '')
-		button.expiration = expiration - GetTime()
+		self.Texture:SetTexture(texture)
+		self.Count:SetText(count > 1 and count or '')
+		self.expiration = expiration - GetTime()
 
-		if(header:GetAttribute('filter') == 'HARMFUL') then
-			local color = DebuffTypeColor[dtype] or DebuffTypeColor.none
-			button:SetBackdropColor(color.r * 3/5, color.g * 3/5, color.b * 3/5)
+		if(self.filter == 'HARMFUL') then
+			local color = DebuffTypeColor[dtype or 'none']
+			self:SetBackdropColor(color.r * 3/5, color.g * 3/5, color.b * 3/5)
 		end
 	end
 end
 
-local function ParseAuras(self, event, unit)
-	if(unit) then
-		if(unit ~= PlayerFrame.unit) then return end
-		if(unit ~= self:GetAttribute('unit')) then
-			self:SetAttribute('unit', unit)
-		end
-	end
-
-	for index = 1, 30 do
-		local child = self:GetAttribute('child' .. index)
-		if(child) then
-			UpdateAuras(self, child)
-		end
+local function OnAttributeChanged(self, attribute, value)
+	if(attribute == 'index') then
+		return UpdateAura(self, value)
 	end
 end
 
-local function CreateAuraHeader(filter, ...)
+local function InitiateAura(self)
+	local Texture = self:CreateTexture(nil, 'BORDER')
+	Texture:SetAllPoints()
+	Texture:SetTexCoord(0.08, 0.92, 0.08, 0.92)
+	self.Texture = Texture
+
+	local Count = self:CreateFontString(nil, 'ARTWORK')
+	Count:SetPoint('BOTTOMRIGHT', -1, 1)
+	Count:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
+	self.Count = Count
+
+	local Duration = self:CreateFontString(nil, 'ARTWORK')
+	Duration:SetPoint('TOPLEFT', 1, -1)
+	Duration:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
+	self.Duration = Duration
+
+	self:SetScript('OnUpdate', UpdateTime)
+	self:SetScript('OnAttributeChanged', OnAttributeChanged)
+	self:SetBackdrop(BACKDROP)
+	self:SetBackdropColor(0, 0, 0)
+end
+
+local function CreateHeader(filter, ...)
 	local header = CreateFrame('Frame', nil, UIParent, 'SecureAuraHeaderTemplate')
-	header:SetPoint(...)
-	header:HookScript('OnEvent', ParseAuras)
-
-	header:SetAttribute('unit', 'player')
-	header:SetAttribute('sortMethod', 'TIME')
 	header:SetAttribute('template', 'ScentAuraTemplate')
+	header:SetAttribute('unit', 'player')
 	header:SetAttribute('filter', filter)
+	header:SetPoint(...)
 
+	header:SetAttribute('sortMethod', 'TIME')
 	header:SetAttribute('point', 'TOPLEFT')
 	header:SetAttribute('minWidth', 330)
 	header:SetAttribute('minHeight', 99)
@@ -78,11 +77,17 @@ local function CreateAuraHeader(filter, ...)
 
 	header:Show()
 
-	return header
+	for index = 1, 30 do
+		local child = self:GetAttribute('child' .. index)
+		if(child) then
+			child.filter = filter
+			InitiateAura(child)
+		end
+	end
 end
 
-ParseAuras(CreateAuraHeader('HELPFUL', 'TOPLEFT', 20, -20))
-ParseAuras(CreateAuraHeader('HARMFUL', 'TOPLEFT', 20, -121))
+CreateHeader('HELPFUL', 'TOPLEFT', 20, -20)
+CreateHeader('HARMFUL', 'TOPLEFT', 20, -121)
 
 BuffFrame:UnregisterAllEvents()
 BuffFrame:Hide()
