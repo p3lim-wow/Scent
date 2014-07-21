79c36b64b952310b058bf6579d7bda34e723ab73
Adrian L Lange <adrianlund@gmail.com>
2014-07-21 04:05:32 +0200

    Kill the temporary enchants
    - It's gone in WoD anyways

--- Scent.lua
+++ Scent.lua
@@ -16,63 +16,45 @@ local function UpdateTime(self, elapsed)
 	end
 end
 
-local function UpdateAura(self, index, enchant)
-	if(enchant) then
-		self.Texture:SetTexture(GetInventoryItemTexture('player', index))
-
-		local count, expiration = select(((index - 15) * 3) - 2, GetWeaponEnchantInfo()) -- Might throw errors, deal with it later
+local function UpdateAura(self, index)
+	local name, _, texture, count, _, duration, expiration = UnitAura(self:GetParent():GetAttribute('unit'), index, 'HELPFUL')
+	if(name) then
+		self.Texture:SetTexture(texture)
 		self.Count:SetText(count > 1 and count or '')
 		self.expiration = expiration - GetTime()
-
-		local quality = GetInventoryItemQuality('player', index)
-		local r, g, b = GetItemQualityColor(quality or 1)
-		self:SetBackdropColor(r, g, b)
-	else
-		local name, _, texture, count, dtype, duration, expiration = UnitAura(self:GetParent():GetAttribute('unit'), index, 'HELPFUL')
-		if(name) then
-			self.Texture:SetTexture(texture)
-			self.Count:SetText(count > 1 and count or '')
-			self.expiration = expiration - GetTime()
-		end
 	end
 end
 
 local function OnAttributeChanged(self, attribute, value)
 	if(attribute == 'index') then
 		UpdateAura(self, value)
-	elseif(attribute == 'target-slot') then
-		UpdateAura(self, value, true)
 	end
 end
 
-local function SkinAura(self)
-	local Texture = self:CreateTexture(nil, 'BORDER')
+local function InitiateAura(self, name, Button)
+	if(not string.match(name, '^child')) then return end
+
+	Button:SetScript('OnUpdate', UpdateTime)
+	Button:SetScript('OnAttributeChanged', OnAttributeChanged)
+	Button:SetBackdrop(BACKDROP)
+	Button:SetBackdropColor(0, 0, 0)
+
+	local Texture = Button:CreateTexture(nil, 'BORDER')
 	Texture:SetAllPoints()
 	Texture:SetTexCoord(0.08, 0.92, 0.08, 0.92)
-	self.Texture = Texture
+	Button.Texture = Texture
 
-	local Count = self:CreateFontString(nil, 'ARTWORK')
+	local Count = Button:CreateFontString(nil, 'ARTWORK')
 	Count:SetPoint('BOTTOMRIGHT', -1, 1)
 	Count:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
-	self.Count = Count
+	Button.Count = Count
 
-	local Duration = self:CreateFontString(nil, 'ARTWORK')
+	local Duration = Button:CreateFontString(nil, 'ARTWORK')
 	Duration:SetPoint('TOPLEFT', 1, -1)
 	Duration:SetFont(FONT, 8, 'OUTLINEMONOCHROME')
-	self.Duration = Duration
+	Button.Duration = Duration
 
-	self:SetScript('OnUpdate', UpdateTime)
-	self:SetScript('OnAttributeChanged', OnAttributeChanged)
-	self:SetBackdrop(BACKDROP)
-	self:SetBackdropColor(0, 0, 0)
-
-	UpdateAura(self, self:GetID())
-end
-
-local function InitiateAura(self, name, value)
-	if(string.match(name, '^child') or string.match(name, '^tempenchant')) then
-		SkinAura(value)
-	end
+	UpdateAura(Button, Button:GetID())
 end
 
 local header = CreateFrame('Frame', 'Scent', UIParent, 'SecureAuraHeaderTemplate')
@@ -80,9 +62,6 @@ header:SetAttribute('template', 'ScentAuraTemplate')
 header:SetAttribute('unit', 'player')
 header:SetAttribute('filter', 'HELPFUL')
 header:SetPoint('TOPLEFT', 20, -20)
-
---header:SetAttribute('includeWeapons', 1)
-header:SetAttribute('weaponTemplate', 'ScentEnchantTemplate')
 
 header:SetAttribute('sortMethod', 'TIME')
 header:SetAttribute('point', 'TOPLEFT')
@@ -97,9 +76,6 @@ RegisterAttributeDriver(header, 'unit', '[vehicleui] vehicle; player')
 
 header:HookScript('OnAttributeChanged', InitiateAura)
 header:Show()
-
-TemporaryEnchantFrame:UnregisterAllEvents()
-TemporaryEnchantFrame:Hide()
 
 BuffFrame:UnregisterAllEvents()
 BuffFrame:Hide()
